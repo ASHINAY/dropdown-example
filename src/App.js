006e9ba687b4ae91b0fcbb5c0b395a9f44851cc8
Nayak <anuja.nayak@capgemini.com>
2023-05-29 16:39:43 +0530

    my task is completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
   const [selectedName, setSelectedName] = useState('');
 
   const handleChange = (event) => {
     setSelectedName(event.target.value);
-};
- function App() {
-    return (
-      <div className="App">
-        <div> Student List</div>
-        <label htmlFor="dropdown">Select a name:</label>
-        <select id="dropdown" value={selectedName} onChange={handleChange}>
-          <option value="">Select</option>
-          <option value="Ram">Ram</option>
-          <option value="Ashi">Ashi</option>
-          <option value="Steven">Steven</option>
-          <option value="Anuja">Anuja</option>
-        </select>
-        <p>Selected name: {selectedName}</p>
-      </div>
-    );
-  }
+  };
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      <label>Student List</label>
+      <select
+        value={selectedName}
+        onChange={handleChange}
+        style={{ width: "100px" }}>
+        <option value="Ram">Ram</option>
+        <option value="Ashi">Ashi</option>
+        <option value="Steven">Steven</option>
+        <option value="Anuja">Anuja</option>
+      </select>
+      <button
+        style={{ width: "100px" }}
+        onClick={() => {
+          alert("Hi " + selectedName);
+        }}
+      >
+        Submit
+      </button>
+    </div>
+  );
 }
 export default App;
