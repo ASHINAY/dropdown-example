121f4888d234ce4845c844d923ab8848cc04a650
Nayak <anuja.nayak@capgemini.com>
2023-05-30 16:42:15 +0530

    create one more dropdown menu

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import './App.css';
 
 function App() {
   const [selectedName, setSelectedName] = useState('');
-
+  const [selectCondition, setselectCondition] = useState('');
+  
   const handleChange = (event) => {
     setSelectedName(event.target.value);
+  };
+
+  const valueChange = (event) => {
+    setselectCondition(event.target.value);
   };
 
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
       <label>Student List</label>
-      <select
-        value={selectedName}
-        onChange={handleChange}
-        style={{ width: "100px" }}>
+      <select value={selectedName} onChange={handleChange} style={{ width: "100px" }}>
         <option value="Ram">Ram</option>
         <option value="Ashi">Ashi</option>
         <option value="Steven">Steven</option>
         <option value="Anuja">Anuja</option>
+      </select>
+      <select value={selectCondition} onChange={valueChange} style={{ width: "100px" }}>
+        <option value="Pass">Pass</option>
+        <option value="Fail">Fail</option>
       </select>
       <button
         style={{ width: "100px" }}
